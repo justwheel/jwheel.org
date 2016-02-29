bc765a0ca67e52dbbf78e4b738d8f4c0b6da237a
Justin W. Flory <me@justinwflory.com>
2016-02-28 22:36:13 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# justinwflory.com
+
+*Website beyond your wildest imagination*
+
+
+## What
+This repository contains the contents of Justin W. Flory's website. The site is primarily used as a general information source for anyone looking to learn more about me as well as bring some more exposure to some of the projects and other things I am working on.
+
+
+## Where
+[justinwflory.com](https://justinwflory.com)
---
+++ update_from_git.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Author: Justin W. Flory
+# Language: Shell
+# Filename: update_from_git.sh
+#
+# Description:
+#    Automatically pulls latest changes from repository
+#    for justinwflory.com from GitHub, deletes old site
+#    data, and copies latest site changes to the virtual
+#    host. The idea is to make updating the site as
+#    painless and simple as possible.
+#
+
+WEBDIR=/var/www/justinwflory.com/public_html
+GITDIR=$HOME/justinwflory.com
+
+#####################################################
+# Pull down the latest changes from GitHub          #
+#####################################################
+cd $GITDIR && git pull
+
+#####################################################
+# Delete all known files in web server directory    #
+#####################################################
+cd $WEBDIR && rm -r ap/ blog/ chat/ contact/ css/ irc/ lit/ music/ old/ index.html robots.txt
+
+#####################################################
+# Copy the files from the git repo into virtualhost #
+#####################################################
+cd $GITDIR
+cp -r ap/ blog/ chat/ contact/ css/ irc/ lit/ music/ old/ index.html robots.txt $WEBDIR
